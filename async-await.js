3603bc6c0f4829a91c15946585b008b6a11ac0ca
Paresh Maniyar <paresh@mobileways.in>
2018-12-18 15:11:08 +0530

    Async Await

--- async-await.js
+++ async-await.js
@@ -1,2 +1,10 @@
 console.log("Hello World");
+(async function() {
+  console.log("Before promise intialisation");
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve({data: "Vashi", value: "Black Sheep"}), 5000);
+  });
+  let result = await promise;
+  console.log("result is "+ JSON.stringify(result));
+})();
 
