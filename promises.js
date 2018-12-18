2104f898b89cea1dc1246374fb078cb8633726ad
Paresh Maniyar <paresh@mobileways.in>
2018-12-18 12:42:35 +0530

    Add promises

---
+++ promises.js
@@ -0,0 +1,35 @@
+console.log("Before initiating promise");
+let promise = new Promise(function(resolve, reject) {
+  console.log("Entered Promise");
+  setTimeout(() => resolve({data: "102", name: "Batman"}), 1000);
+  console.log("Below Resolve");
+});
+console.log("Before calling promise.then");
+promise.then(
+  (data) => {
+    console.log("After Promises in .Then of resolve");
+    console.log(JSON.stringify(data));
+  },
+  (rejectError) => {
+    console.log("Hope this wont be called "+ JSON.stringify(rejectError));
+  }
+);
+console.log("After calling promise///////////////////////////");
+//Reject error throwing
+console.log("Before initiating promise");
+let promise2 = new Promise(function(resolve, reject) {
+  console.log("Entered Promise");
+  setTimeout(() => reject(new Error("Error Message")), 1000);
+  console.log("Below Resolve");
+});
+console.log("Before calling promise.then");
+promise2.then(
+  (data) => {
+    console.log("After Promises in .Then of resolve");
+    console.log(JSON.stringify(data));
+  },
+  (rejectError) => {
+    console.log("Hope this will be called "+ rejectError);
+  }
+);
+console.log("After calling promise");
