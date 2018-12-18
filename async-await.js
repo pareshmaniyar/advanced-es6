ecb3422f91c1d0bda6fd8a10ff5f124c139e718b
Paresh Maniyar <paresh@mobileways.in>
2018-12-18 18:16:54 +0530

    Add async await error/reject response

--- async-await.js
+++ async-await.js
@@ -2,9 +2,22 @@ console.log("Hello World");
 (async function() {
   console.log("Before promise intialisation");
   let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve({data: "Vashi", value: "Black Sheep"}), 5000);
+    setTimeout(() => resolve({data: "Vashi", value: "Black Sheep"}), 500);
   });
   let result = await promise;
   console.log("result is "+ JSON.stringify(result));
+})();
+
+(async function() {
+  console.log("Error function started");
+  try {
+    let promise = new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error("Why so serious?")), 500);
+    });
+    let result = await promise;
+    console.log("won't print this", result);
+  } catch (err) {
+    console.log("err is:", err);
+  }
 })();
 
