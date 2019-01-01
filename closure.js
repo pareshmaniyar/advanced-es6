a45b63c1ac50ff3bc38be314553e43de1f665d71
Mann <pareshmaniyar1@gmail.com>
2019-01-01 19:29:43 +0530

    Add closure example

--- closure.js
+++ closure.js
@@ -16,6 +16,29 @@ function makeAdder(x) {
   }
 }
 
-const increament = makeAdder(1);
-const x = increament(9);
+const increment = makeAdder(1);
+const x = increment(9);
 console.log(x);
+
+var counter = (function() {
+  let _private_count = 0;
+  function inc(x) {
+    _private_count = _private_count + x;
+  }
+  return {
+    incOne: () => {
+      inc(1);
+    },
+    decOne: () => {
+      inc(-1);
+    },
+    value: () => _private_count
+  }
+})();
+
+console.log(counter.value());
+counter.incOne();
+counter.incOne();
+console.log(counter.value());
+counter.decOne();
+console.log(counter.value());
