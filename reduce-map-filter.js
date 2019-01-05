4a3f04859464739788bacfa10e637e7cabd5ba53
Mann <pareshmaniyar1@gmail.com>
2019-01-05 17:15:07 +0530

    Add forEach example

--- reduce-map-filter.js
+++ reduce-map-filter.js
@@ -13,10 +13,15 @@ newArray = revArray.map(x => {//one to one mapping, it has to return something
 console.log(newArray);
 
 let arr = [1,2,3,4,5,6,7,8,9,10];
-let total10 = arr.reduce((total, x) => x+total, 10);
+let total10 = arr.reduce((total, x) => x+total, 10);///////////////
 console.log(total10);
 
-let greaterThanFiveArray = arr.filter(x => {
+let greaterThanFiveArray = arr.filter(x => {///////////////////
   if(x>5) return x;
 });
 console.log(greaterThanFiveArray);
+console.log('Before forEach: ', arr);
+arr.forEach((value, index) => {
+  if(value%2 !== 0) arr[index] = value*2;
+});
+console.log('After forEach: ', arr);
