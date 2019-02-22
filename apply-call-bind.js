c691c45470bcbd139ca7986f6e0165825bd8230f
Mann <pareshmaniyar1@gmail.com>
2019-02-22 10:45:22 +0530

    Modify bind, apply, call and add string

--- apply-call-bind.js
+++ apply-call-bind.js
@@ -1,31 +1,32 @@
+//Sources: https://stackoverflow.com/questions/15455009/javascript-call-apply-vs-bind
 // for reference
 var pokemon = {
   firstname: 'Pika',
-  lastname: 'Chu ',
+  lastname: 'Chu',
   getPokeName: function() {
       var fullname = this.firstname + ' ' + this.lastname;
       return fullname;
   }
 };
-
-var pokemonName = function() {
-  console.log(this.getPokeName() + 'I choose you!');
+var changePokemonName = function() {
+  this.firstname = 'Pikaa';
+  console.log(this);//points to binded object
+  return 'done';
 };
-pokemon.firstname = 'Pikaa';
-var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
-console.log(logPokemon);
-logPokemon(); // 'Pika Chu I choose you!'
-pokemon.firstname = 'Jika';
-//When we use the bind() method:
-//the JS engine is creating a new pokemonName instance and binding pokemon as its this variable. It is important to understand that it copies the pokemonName function.
-//After creating a copy of the pokemonName function it is able to call logPokemon(), although it wasn’t on the pokemon object initially. It will now recognizes its properties (Pika and Chu) and its methods.
+var logPokemon = changePokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
+console.log(logPokemon);//[Function: bound pokemonName]
+logPokemon();/**{ firstname: 'Pikaa',
+lastname: 'Chu ',
+getPokeName: [Function: getPokeName] } */
+console.log(logPokemon()); /** { firstname: 'Pikaa',
+lastname: 'Chu ',
+getPokeName: [Function: getPokeName] }
+done */
+
 var pokemonName2 = function(snack, hobby) {
   console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
 };
 
-pokemonName2.call(pokemon,'sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
-pokemonName2.apply(pokemon,['sushi', 'algorithms']); // Pika Chu  loves sushi and algorithms
+pokemonName2.call(pokemon,'Pani puri', 'gives shocking news'); // Pikaa Chu loves Pani puri and gives shocking news
+pokemonName2.apply(pokemon,['Pani puri', 'loves spending time with ash ketchum']); // Pikaa Chu loves Pani puri and loves spending time with ash ketchum
 
-str = 'abcdefg';
-revStr = str.split('').reverse().join("");
-console.log(revStr);
---
+++ string.js
@@ -0,0 +1,17 @@
+str = 'abcdefg';
+strSplit = str.split('');
+console.log(strSplit);//[ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]
+strSplit2 = str.split('d');
+console.log(strSplit2);//[ 'abc', 'efg' ]
+strRev2 = strSplit2.reverse();
+console.log(strRev2);
+strJoin = strRev2.join("d");
+console.log(strJoin);
+strJoin2 = strRev2.join("a");
+console.log(strJoin2);
+strJoin3 = strRev2.join("ga");
+console.log(strJoin3);
+strRev = strSplit.reverse();
+console.log(strRev);//[ 'g', 'f', 'e', 'd', 'c', 'b', 'a' ]
+revStr = str.split('').reverse().join("x");
+console.log(revStr + ' with xin b/w');
