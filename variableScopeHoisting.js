a5a0c80db75ba88c92aef20200a186c68d01e05a
Paresh Maniyar <pareshmaniyar1@gmail.com>
2021-12-02 11:44:15 +0530

    Add fundamental concepts

---
+++ variableScopeHoisting.js
@@ -0,0 +1,28 @@
+/*
+let, const
+- not hoisted
+- redeclaration not possible
+
+var
+- hoisted
+- redeclaration possible
+
+*/
+// console.log(a);//not defined
+// console.log(f);//not defined
+let a = "a";
+const f = "f";
+// let a = "a changed"; // not accepted
+console.log(a);//a
+console.log(b);//undefined
+console.log(c);//undefined
+var b = "b";
+var b = "b changed"; // accepted
+console.log(b);
+if(true) {
+    var c = "c";
+    let d = "d";
+}
+console.log(c); //c
+// console.log(d); // not defined
+
