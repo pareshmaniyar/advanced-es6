e3deefd9c79c2eff5aafb96d9baaac0c5f42c5fc
Paresh Maniyar <pareshmaniyar1@gmail.com>
2021-12-10 19:02:46 +0530

    Add kenko health interview

--- kenkoHealth.js
+++ kenkoHealth.js
@@ -68,3 +68,120 @@ function convertion2(s) {
     return result.trim();
 }
 console.log(output === convertion2(input1));
+
+/*
+
+- Given an arr, k. return largest 
+[1,2,3]
+[1.11, 5.84, 4.89, 3.03, 7.09], 3
+[5.84, 4.89, 7.09]
+[1,2,1,2,2]
+
+New approach
+BF:
+- n*k
+- n*logn
+[5,4,1,2,3]
+- storing indexes
+1. sorting - []
+2. sorting based on index
+
+- arr = sorted, int
+- k < arr.length
+[4,5,1,2,3] , 3
+[4,5,3]
+
+n*k
+     3
+    / \
+   5   
+[3,5,4]
+- get max k
+
+function Node (val, index){
+  this.val = val;
+  this.index = index;
+}
+
+BF:
+nlogk + klogk
+heap - min
+- loop through O(n)
+  - adding each heap - O(k)
+  - heap size > k, last element
+
+O(1)
+
+MongoDB
+- Students
+- Groups - subscribe
+
+- On event push notification to group
+- pass Data to Notification service
+
+1 - student has groupID
+student -> 
+
+
+
+
+
+
+2 - groups has arr of studentID
+event -> groupdID -> arr students
+
+
+
+groups - 50
+student - 5 million
+1 group - 50 million
+
+
+case 1
+1 
+
+2 
+
+
+case 2
+
+
+
+/////
+API - 
+- t data points
+- x data points in collection a
+- t - x data points in collection b
+
+x subset of t
+
+first a, then b
+API
+caching id - 10 million
+
+- if put x in a
+
+
+A + B
+
+mongoose.populate 
+
+
+
+
+
+
+
+*/
+
+function largestKArr(arr, k) {
+    const map = {};
+    for(let i = 0; i < arr.length; i++) {
+      map[arr[i]] = i;
+    }
+    arr.sort((a, b) => {});
+    let result = new Array(k);
+    return result;
+  }
+  
+  
